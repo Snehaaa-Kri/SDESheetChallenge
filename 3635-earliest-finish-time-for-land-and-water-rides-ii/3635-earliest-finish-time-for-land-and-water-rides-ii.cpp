76462edc6f4aa3836da5867ee9135d1131d02d2a
Sneha kumari <137854752+Snehaaa-Kri@users.noreply.github.com>
2026-06-03 20:41:04 +0530

    Time: 3 ms (78.9%), Space: 238 MB (66.06%) - LeetHub

---
+++ 3635-earliest-finish-time-for-land-and-water-rides-ii/3635-earliest-finish-time-for-land-and-water-rides-ii.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int  findFT(vector<int>& start1, vector<int>& duration1, vector<int>& start2, vector<int>& duration2){
+        int finish1 = INT_MAX;
+
+        for(int i=0; i<start1.size(); i++){
+            finish1 = min(finish1, start1[i] + duration1[i]);
+        }
+
+        int finish2 = INT_MAX;
+        for(int i=0; i<start2.size(); i++){
+            finish2 = min(finish2, max(start2[i], finish1) + duration2[i]);
+        }
+
+        return finish2;
+    }
+
+
+    int earliestFinishTime(vector<int>& landStartTime, vector<int>& landDuration, vector<int>& waterStartTime, vector<int>& waterDuration) {
+        int op1 = findFT(landStartTime, landDuration, waterStartTime, waterDuration);
+
+        int op2 = findFT(waterStartTime, waterDuration, landStartTime, landDuration);
+
+        return min(op1, op2);
+    }
+};
