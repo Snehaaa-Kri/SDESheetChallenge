680837f96de037f70057ea2597ebcdcb55a733ec
Sneha kumari <137854752+Snehaaa-Kri@users.noreply.github.com>
2026-06-17 10:11:37 +0530

    Time: 4 ms (63.88%), Space: 39.6 MB (87.45%) - LeetHub

---
+++ 3612-process-string-with-special-operations-i/3612-process-string-with-special-operations-i.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    string processStr(string s) {
+        string curr = "";
+
+        for(auto ch: s){
+            if(ch == '*'){ if(curr.size()>0) curr.pop_back();}
+            else if(ch == '#') curr += curr;
+            else if(ch == '%') reverse(curr.begin(), curr.end());
+            else curr += ch;
+            
+        }
+        return curr;
+    }
+};
