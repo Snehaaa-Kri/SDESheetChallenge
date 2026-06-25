d9b6a3e32c6b7ed15f7270b42e0b42e53d63c724
Sneha kumari <137854752+Snehaaa-Kri@users.noreply.github.com>
2026-06-25 21:47:14 +0530

    Time: 0 ms (100%), Space: 8.7 MB (89.03%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+//stack
+    bool isValid(string s) {
+        stack<char> st;
+
+        for(int i=0; i<s.size(); i++){
+            if(s[i] == '(' || s[i] == '{' || s[i] == '[') st.push(s[i]);
+            else {
+                if(s[i] ==')' && !st.empty() && st.top() == '(') st.pop();
+                else if(s[i] == '}' && !st.empty() && st.top() == '{') st.pop();
+                else if(s[i] == ']' && !st.empty() && st.top() == '[') st.pop();
+                else return false;
+            }
+        }
+        return st.empty();
+    }
+};
