7e7a76556d057f20ac1988bf104ef625f7848d48
Sneha kumari <137854752+Snehaaa-Kri@users.noreply.github.com>
2026-07-02 23:00:30 +0530

    Time: 2 ms (6.78%), Space: 8.7 MB (17.46%) - LeetHub

---
+++ 0165-compare-version-numbers/0165-compare-version-numbers.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    int compareVersion(string version1, string version2) {
+        vector<int> v1, v2;
+        string temp = "";
+
+        for (char c : version1) {
+            if (c == '.') {
+                v1.push_back(stoi(temp));
+                temp = "";
+            } else {
+                temp += c;
+            }
+        }
+        v1.push_back(stoi(temp));
+
+        temp = "";
+        for (char c : version2) {
+            if (c == '.') {
+                v2.push_back(stoi(temp));
+                temp = "";
+            } else {
+                temp += c;
+            }
+        }
+        v2.push_back(stoi(temp));
+
+        int length = max(v1.size(), v2.size());
+
+        for (int i = 0; i < length; ++i) {
+            int num1 = i < v1.size() ? v1[i] : 0;
+            int num2 = i < v2.size() ? v2[i] : 0;
+
+            if (num1 > num2) return 1;
+            if (num1 < num2) return -1;
+        }
+
+        return 0;
+    }
+};
