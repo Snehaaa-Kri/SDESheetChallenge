9104ac3d06c7c86a708d9d92b9cba87d5fce99dc
Sneha kumari <137854752+Snehaaa-Kri@users.noreply.github.com>
2026-07-30 14:50:56 +0530

    Time: 2 ms (16.1%), Space: 8.4 MB (31.81%) - LeetHub

---
+++ 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int minimumPushes(string word) {
+        int ans[] = {0, 1, 2, 3, 4, 5, 6, 7,8,10, 12, 14, 16, 18, 20, 22, 24, 27, 30, 33, 36, 39, 42, 45, 48, 52, 56};
+
+
+        return ans[word.size()];
+
+
+    }
+};
